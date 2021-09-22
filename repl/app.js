8c3fcfcfc8c5c102ad4c8ba69bbc41990d627131
qqinys <shinein@126.com>
2021-09-22 18:53:53 +0800

    change sen

--- repl/app.js
+++ repl/app.js
@@ -67,8 +67,8 @@ class App {
         this.#talentExtend = localStorage.talentExtend;
         await this.#life.initial();
         this.output(`\rLoading Complete.
-人生重开模拟器
-这垃圾人生一秒也不想待了
+大学重开模拟器
+我随便改一个看看能不能变？
 \n🎉键入 \x1B[4m/remake\x1B[24m 开始游戏`,
             true
         );
